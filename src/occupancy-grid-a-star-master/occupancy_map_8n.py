02959d42141aca6482f60f3bfa7417394ca8d0e9
Naren <narendas@andrew.cmu.edu>
2022-02-25 20:27:05 -0500

    converted pgm to graph

---
+++ src/occupancy-grid-a-star-master/occupancy_map_8n.py
@@ -0,0 +1,63 @@
+from tracemalloc import start
+from gridmap import OccupancyGridMap
+import matplotlib.pyplot as plt
+from a_star import a_star
+from utils import plot_path
+import os
+from PIL import Image, ImageOps
+import numpy as np
+def define_point(node,gmap):
+    node = gmap.get_index_from_coordinates(node[0], node[1])
+    if gmap.is_occupied_idx(node):
+        node = list(node)
+        node[0] = node[0] + 1
+        node[1] = node[1] + 1
+        node = tuple(node)
+        define_point(node,gmap)
+    else:
+        return node
+def createGraph(n, nodes,gmap):
+    adj = np.zeros((n, n))
+    nodes = np.array(nodes)
+    for i in range(n):
+        for j in range(i+1,n):
+            start_n = tuple(nodes[i,:])
+            stop_n = tuple(nodes[j,:])
+            path,path_px,adj[i,j] = a_star(start_n, stop_n, gmap, movement='8N')
+            adj[j, i] = adj[i,j]
+            gmap.plot()
+
+            if path:
+                # plot resulting path in pixels over the map
+                plot_path(path_px)
+                print("hi")
+            else:
+                print('Goal is not reachable')
+
+                # plot start and goal points over the map (in pixels)
+                start_node_px = gmap.get_index_from_coordinates(nodes[i][0], nodes[i][1])
+                goal_node_px = gmap.get_index_from_coordinates(nodes[j][0], nodes[j][1])
+
+                plt.plot(start_node_px[0], start_node_px[1], 'ro')
+                plt.plot(goal_node_px[0], goal_node_px[1], 'go')
+
+    plt.show()
+    return adj
+def set_of_nodes(gmap):
+    start_node = np.zeros([3,2])
+    start_node[0,:] = [0.0,0.0]
+    for i in range(1,3):
+        temp=tuple(start_node[i-1,:])
+        start_node[i] = list(define_point((temp[0]+10,temp[1]+10),gmap))
+    return tuple(start_node)
+
+if __name__ == '__main__':
+    # load the map
+    filename = 'maps/localization_map_vicon.pgm'
+    new_file = "{}.png".format(filename)
+    with Image.open(filename) as im:
+        im = ImageOps.invert(im)
+        im.save(new_file)
+    gmap = OccupancyGridMap.from_png(new_file, 1)
+    nodes = list(set_of_nodes(gmap))
+    adj = createGraph(3,nodes,gmap)
