744b541e7b21a68616aded8c6c5d7ca77b2faf55
Rachel Zheng <rachelzh@andrew.cmu.edu>
2022-10-11 04:10:51 -0400

    initial filter code

--- src/frontier_exploration/scripts/functions.py
+++ src/frontier_exploration/scripts/functions.py
@@ -1,91 +1,15 @@
-import rospy
-import tf
+#!/usr/bin/env python3
+
 from numpy import array
-import actionlib
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from nav_msgs.srv import GetPlan
-from geometry_msgs.msg import PoseStamped
 from numpy import floor
 from numpy.linalg import norm
 from numpy import inf
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+
 # ________________________________________________________________________________
-
-
-class robot:
-    goal = MoveBaseGoal()
-    start = PoseStamped()
-    end = PoseStamped()
-
-    def __init__(self, name):
-        self.assigned_point = []
-        self.name = name
-        self.global_frame = rospy.get_param('~global_frame', '/map')
-        self.robot_frame = rospy.get_param('~robot_frame', 'base_link')
-        self.plan_service = rospy.get_param(
-            '~plan_service', '/move_base_node/NavfnROS/make_plan')
-        self.listener = tf.TransformListener()
-        self.listener.waitForTransform(
-            self.global_frame, self.name+'/'+self.robot_frame, rospy.Time(0), rospy.Duration(10.0))
-        cond = 0
-        while cond == 0:
-            try:
-                rospy.loginfo('Waiting for the robot transform')
-                (trans, rot) = self.listener.lookupTransform(
-                    self.global_frame, self.name+'/'+self.robot_frame, rospy.Time(0))
-                cond = 1
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                cond == 0
-        self.position = array([trans[0], trans[1]])
-        self.assigned_point = self.position
-        self.client = actionlib.SimpleActionClient(
-            self.name+'/move_base', MoveBaseAction)
-        self.client.wait_for_server()
-        robot.goal.target_pose.header.frame_id = self.global_frame
-        robot.goal.target_pose.header.stamp = rospy.Time.now()
-
-        rospy.wait_for_service(self.name+self.plan_service)
-        self.make_plan = rospy.ServiceProxy(
-            self.name+self.plan_service, GetPlan)
-        robot.start.header.frame_id = self.global_frame
-        robot.end.header.frame_id = self.global_frame
-
-    def getPosition(self):
-        cond = 0
-        while cond == 0:
-            try:
-                (trans, rot) = self.listener.lookupTransform(
-                    self.global_frame, self.name+'/'+self.robot_frame, rospy.Time(0))
-                cond = 1
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                cond == 0
-        self.position = array([trans[0], trans[1]])
-        return self.position
-
-    def sendGoal(self, point):
-        robot.goal.target_pose.pose.position.x = point[0]
-        robot.goal.target_pose.pose.position.y = point[1]
-        robot.goal.target_pose.pose.orientation.w = 1.0
-        self.client.send_goal(robot.goal)
-        self.assigned_point = array(point)
-
-    def cancelGoal(self):
-        self.client.cancel_goal()
-        self.assigned_point = self.getPosition()
-
-    def getState(self):
-        return self.client.get_state()
-
-    def makePlan(self, start, end):
-        robot.start.pose.position.x = start[0]
-        robot.start.pose.position.y = start[1]
-        robot.end.pose.position.x = end[0]
-        robot.end.pose.position.y = end[1]
-        start = self.listener.transformPose(self.name+'/map', robot.start)
-        end = self.listener.transformPose(self.name+'/map', robot.end)
-        plan = self.make_plan(start=start, goal=end, tolerance=0.0)
-        return plan.plan.poses
-# ________________________________________________________________________________
-
 
 def index_of_point(mapData, Xp):
     resolution = mapData.info.resolution
@@ -96,7 +20,7 @@ def index_of_point(mapData, Xp):
     index = int((floor((Xp[1]-Xstarty)/resolution) *
                  width)+(floor((Xp[0]-Xstartx)/resolution)))
     return index
-
+# ________________________________________________________________________________
 
 def point_of_index(mapData, i):
     y = mapData.info.origin.position.y + \
